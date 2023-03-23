df3d98e6e0453defd87bb9f2a625516e4f90b084
Mang-Joo <wjwan0915@gmail.com>
2023-03-23 23:25:03 +0900

    :white_check_mark: test: Test용 Controller

--- src/main/java/com/mangjoo/io/securityexample/controller/TestController.java
+++ src/main/java/com/mangjoo/io/securityexample/controller/TestController.java
@@ -1,19 +1,20 @@
 package com.mangjoo.io.securityexample.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
-
 @RestController
+@RequestMapping("/api/v1/user")
 public class TestController {
 
     @GetMapping("/test")
-    public void test(@AuthenticationPrincipal Principal principal) {
+    public ResponseEntity<String> test(@AuthenticationPrincipal Long id) {
         System.out.println("test");
-        System.out.println("principal.getName(); = " + principal.getName());
-
+        System.out.println("principal.getName(); = " + id);
+        return ResponseEntity.ok(id.toString());
     }
 
 }
